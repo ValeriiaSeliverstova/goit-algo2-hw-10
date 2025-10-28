81b1523eac1eeade93648615a9efd1cc22966867
seliverstova-valerya <seliverstova.valerya@gmail.com>
2025-10-28 21:44:26 +0200

    feat: hw10

---
+++ task1.py
@@ -0,0 +1,89 @@
+# Визначення класу Teacher
+class Teacher:
+    def __init__(self, first_name, last_name, age, email, can_teach_subjects):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.email = email
+        self.can_teach_subjects = can_teach_subjects
+        self.assigned_subjects = []
+
+
+def create_schedule(subjects, teachers):
+    uncovered = set(subjects)
+    for teacher in teachers:
+        teacher.assigned_subjects = []
+    schedule = []
+
+    while uncovered:
+        best_teacher = None
+        best_coverage = set()
+
+        for teacher in teachers:
+            coverage = uncovered.intersection(teacher.can_teach_subjects)
+
+            if not coverage:
+                continue
+
+            if len(coverage) > len(best_coverage) or (
+                len(coverage) == len(best_coverage) and teacher.age < best_teacher.age
+            ):
+                best_teacher = teacher
+                best_coverage = coverage
+
+        if not best_teacher:
+            return None
+
+        best_teacher.assigned_subjects.extend(best_coverage)
+        uncovered -= best_coverage
+        schedule.append(best_teacher)
+
+    return schedule
+
+
+if __name__ == "__main__":
+    # Множина предметів
+    subjects = {"Математика", "Фізика", "Хімія", "Інформатика", "Біологія"}
+    # Створення списку викладачів
+    teachers = [
+        Teacher(
+            "Олександр",
+            "Іваненко",
+            45,
+            "o.ivanenko@example.com",
+            {"Математика", "Фізика"},
+        ),
+        Teacher("Марія", "Петренко", 38, "m.petrenko@example.com", {"Хімія"}),
+        Teacher(
+            "Сергій",
+            "Коваленко",
+            50,
+            "s.kovalenko@example.com",
+            {"Інформатика", "Математика"},
+        ),
+        Teacher(
+            "Наталія", "Шевченко", 29, "n.shevchenko@example.com", {"Біологія", "Хімія"}
+        ),
+        Teacher(
+            "Дмитро",
+            "Бондаренко",
+            35,
+            "d.bondarenko@example.com",
+            {"Фізика", "Інформатика"},
+        ),
+        Teacher("Олена", "Гриценко", 42, "o.grytsenko@example.com", {"Біологія"}),
+    ]
+
+    # Виклик функції створення розкладу
+    schedule = create_schedule(subjects, teachers)
+
+    # Виведення розкладу
+    if schedule:
+        print("Розклад занять:")
+        for teacher in schedule:
+            print(
+                f"{teacher.first_name} {teacher.last_name}, {teacher.age} років, email: {teacher.email}"
+            )
+            print(f"   Викладає предмети: {', '.join(teacher.assigned_subjects)}\\n")
+    else:
+        print("Неможливо покрити всі предмети наявними викладачами.")
